b8d1285f8faca15bdffa00ea735fe3811cccd121
Gonzalo Castro <gcastro0690@gmail.com>
2021-08-22 12:31:57 -0300

    review rating

--- src/components/ItemInfo.jsx
+++ src/components/ItemInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Toggle } from '.';
 import Button from './Button';
+import Rating from '@material-ui/lab/Rating';
 
 const ItemInfo = ({
   items,
@@ -16,6 +17,14 @@ const ItemInfo = ({
       <span className="fw-bold">{selectedItem.name}</span>
       <span>{selectedItem.price}</span>
     </div>
+    <div className="itemInfo__reviewScore d-flex justify-content-between align-items-center w-100">
+      <span className="fw-bold">Review Rating</span>
+      <div className="d-flex no-wrap align-items-center">
+        <span className="me-1">{`(${selectedItem.reviewScore})`}</span>
+        <Rating value={selectedItem.reviewScore} readOnly={true} precision={0.5}/>
+      </div>
+    </div>
+
     <Button addToCart={() => incrementCartCount(cartCount + 1)} />
   </div>
 );
--- src/views/ItemView.jsx
+++ src/views/ItemView.jsx
@@ -7,18 +7,21 @@ const items = [
     name: 'Saatva classic',
     price: '$999',
     image: 'images/classic-carousel.jpg',
+    reviewScore: 4.5
   },
   {
     id: 2,
     name: 'Loom & Leaf',
     price: '$1.299',
     image: 'images/loom-carousel.jpg',
+    reviewScore: 1
   },
   {
     id: 3,
     name: 'Zenhaven',
     price: '$1.599',
     image: 'images/zen-carousel.jpg',
+    reviewScore: 1.5
   },
 ];
 
